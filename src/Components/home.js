13bd34387ed6d1fbdb49fb01e97b1768fc82685d
rohan-gov <30332075+rohan-gov@users.noreply.github.com>
2022-01-27 07:55:56 +0530

    initial commit

---
+++ src/Components/home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <header className="jumbotron">
+        <h3>
+          <strong>User home page</strong>
+        </h3>
+      </header>
+    </div>
+  );
+};
+
+export default Home;
